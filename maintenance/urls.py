a593f4b91c45f254ae0a42e211f16e56dd8056ea
Vahidvhd <vahidvhd91@gmail.com>
2026-07-20 19:56:43 +0100

    Rename property API route to properties

--- maintenance/urls.py
+++ maintenance/urls.py
@@ -2,7 +2,7 @@ from rest_framework.routers import DefaultRouter
 from .views import PropertyViewSet, MaintenanceRequestViewSet
 
 router = DefaultRouter()
-router.register('property', PropertyViewSet, basename='property')
+router.register('properties', PropertyViewSet, basename='property')
 router.register('requests', MaintenanceRequestViewSet, basename='request')
 
 urlpatterns = router.urls
